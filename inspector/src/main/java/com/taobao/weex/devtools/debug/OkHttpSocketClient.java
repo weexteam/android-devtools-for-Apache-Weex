b6a3a8f9d33562726e64a982280f0fcb9d21ddbd
翟霄宇 <zhaixiaoyu@alibaba-inc.com>
2017-11-24 00:53:09 +0800

    support okhttp 275

--- inspector/src/main/java/com/taobao/weex/devtools/debug/OkHttpSocketClient.java
+++ inspector/src/main/java/com/taobao/weex/devtools/debug/OkHttpSocketClient.java
@@ -1,12 +1,5 @@
 package com.taobao.weex.devtools.debug;
 
-import android.text.TextUtils;
-import android.util.Log;
-
-import com.taobao.weex.devtools.common.LogRedirector;
-import com.taobao.weex.devtools.common.ReflectionUtil;
-import com.taobao.weex.utils.WXLogUtils;
-
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
@@ -14,6 +7,12 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
+
+import android.text.TextUtils;
+import android.util.Log;
+import com.taobao.weex.devtools.common.LogRedirector;
+import com.taobao.weex.devtools.common.ReflectionUtil;
+import com.taobao.weex.utils.WXLogUtils;
 
 
 public class OkHttpSocketClient extends SocketClient {
@@ -35,6 +34,11 @@ public class OkHttpSocketClient extends SocketClient {
   private static final String CLASS_BUFFER = "okio.Buffer";
   private static final String CLASS_BUFFER_SOURCE = "okio.BufferedSource";
 
+  //okhttp 2.7.5
+  private static final String CLASS_MEDIATYPE = "com.squareup.okhttp.MediaType";
+  private static final String CLASS_REQUESTBODY = "com.squareup.okhttp.RequestBody";
+  private static final String CLASS_RESPONSEBODY = "com.squareup.okhttp.ResponseBody";
+
   static {
     String[] classNames = new String[]{
         CLASS_WEBSOCKET,
@@ -46,7 +50,10 @@ public class OkHttpSocketClient extends SocketClient {
         CLASS_REQUEST,
         CLASS_REQUEST_BUILDER,
         CLASS_BUFFER,
-        CLASS_BUFFER_SOURCE
+        CLASS_BUFFER_SOURCE,
+        CLASS_MEDIATYPE,
+        CLASS_REQUESTBODY,
+        CLASS_RESPONSEBODY
     };
     for (String className : classNames) {
       sClazzMap.put(className, ReflectionUtil.tryGetClassForName(className));
@@ -59,11 +66,16 @@ public class OkHttpSocketClient extends SocketClient {
   private Class mWebSocketCallClazz = sClazzMap.get(CLASS_WEBSOCKET_CALL);
   private Class mWebSocketListenerClazz = sClazzMap.get(CLASS_WEBSOCKET_LISTENER);
 
-  private Class mMediaTypeClazz = sClazzMap.get(CLASS_WEBSOCKET_PAYLOADTYPE);
+  private Class mPayloadTypeClazz = sClazzMap.get(CLASS_WEBSOCKET_PAYLOADTYPE);
   private Class mWebSocketClazz = sClazzMap.get(CLASS_WEBSOCKET);
 
   private Class mBufferClazz = sClazzMap.get(CLASS_BUFFER);
   private Class mBufferedSourceClazz = sClazzMap.get(CLASS_BUFFER_SOURCE);
+
+  private Class mMediaTypeClazz = sClazzMap.get(CLASS_MEDIATYPE);
+
+  private Class mRequestBodyClazz = sClazzMap.get(CLASS_REQUESTBODY);
+  private Class mResponseBodyClazz = sClazzMap.get(CLASS_RESPONSEBODY);
 
   public OkHttpSocketClient(DebugServerProxy proxy) {
     super(proxy);
@@ -155,25 +167,37 @@ public class OkHttpSocketClient extends SocketClient {
       return;
     }
     try {
-      Field textField;
-      Object textValue;
-      try {
-        textField = ReflectionUtil.tryGetDeclaredField(mMediaTypeClazz, "TEXT");
-        textValue = ReflectionUtil.getFieldValue(textField, null);
-      } catch (Exception e) {
-        textField = ReflectionUtil.tryGetDeclaredField(mWebSocketClazz, "TEXT");
-        textValue = ReflectionUtil.getFieldValue(textField, null);
+      if (mPayloadTypeClazz != null) {
+        Field textField = ReflectionUtil.tryGetDeclaredField(mPayloadTypeClazz, "TEXT");
+        Object textValue = ReflectionUtil.getFieldValue(textField, null);
+        Method sendMessageMethod = ReflectionUtil.tryGetMethod(mWebSocketClazz,
+            "sendMessage", new Class[] {mPayloadTypeClazz, mBufferClazz});
+
+        Object buffer = mBufferClazz.newInstance();
+        Method writeUtf8 = ReflectionUtil.tryGetMethod(mBufferClazz, "writeUtf8",
+            new Class[] {String.class});
+
+        ReflectionUtil.tryInvokeMethod(mWebSocket, sendMessageMethod, textValue,
+            ReflectionUtil.tryInvokeMethod(buffer, writeUtf8, message));
+      } else {
+        //okhttp 2.7.5
+        Field textField = ReflectionUtil.tryGetDeclaredField(mWebSocketClazz, "TEXT");
+        Object textValue = ReflectionUtil.getFieldValue(textField, null);
+
+        Method createMethod = ReflectionUtil.tryGetMethod(
+            mRequestBodyClazz,
+            "create",
+            new Class[] {mMediaTypeClazz, String.class});
+
+        Object requestBody = ReflectionUtil.tryInvokeMethod(
+            mRequestBodyClazz,
+            createMethod,
+            textValue,
+            message);
+        Method sendMessageMethod = ReflectionUtil.tryGetMethod(mWebSocketClazz,
+            "sendMessage", new Class[] {mRequestBodyClazz});
+        ReflectionUtil.tryInvokeMethod(mWebSocket, sendMessageMethod, requestBody);
       }
-
-      Method sendMessageMethod = ReflectionUtil.tryGetMethod(mWebSocketClazz,
-          "sendMessage", new Class[]{mMediaTypeClazz, mBufferClazz});
-
-      Object buffer = mBufferClazz.newInstance();
-      Method writeUtf8 = ReflectionUtil.tryGetMethod(mBufferClazz, "writeUtf8",
-          new Class[]{String.class});
-
-      ReflectionUtil.tryInvokeMethod(mWebSocket, sendMessageMethod, textValue,
-          ReflectionUtil.tryInvokeMethod(buffer, writeUtf8, message));
     } catch (IllegalAccessException e) {
       e.printStackTrace();
     } catch (InstantiationException e) {
@@ -205,10 +229,18 @@ public class OkHttpSocketClient extends SocketClient {
       } else if ("onFailure".equals(method.getName())) {
         abort("Websocket onFailure", (IOException) args[0]);
       } else if ("onMessage".equals(method.getName())) {
-        Object bufferedSource = mBufferedSourceClazz.cast(args[0]);
+        Object bufferedSource;
+        if (mPayloadTypeClazz != null) {
+          bufferedSource = mBufferedSourceClazz.cast(args[0]);
+        } else {
+          //okhttp 2.7.5
+          Object responseBody = mResponseBodyClazz.cast(args[0]);
+          Method sourceMethod = ReflectionUtil.tryGetMethod(mResponseBodyClazz, "source");
+          bufferedSource = ReflectionUtil.tryInvokeMethod(responseBody, sourceMethod);
+        }
         Method readUtf8 = ReflectionUtil.tryGetMethod(mBufferedSourceClazz, "readUtf8");
         try {
-          String message = (String) ReflectionUtil.tryInvokeMethod(bufferedSource, readUtf8);
+          String message = (String)ReflectionUtil.tryInvokeMethod(bufferedSource, readUtf8);
           mProxy.handleMessage(message);
         } catch (Exception e) {
           if (LogRedirector.isLoggable(TAG, Log.VERBOSE)) {
